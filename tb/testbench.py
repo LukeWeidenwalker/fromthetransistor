8c94d180ff608aa476bbcde055cf336cbf1aa403
Lukas Weidenholzer <lukas.weidenholzer@gmail.com>
2022-10-01 12:08:36 +0200

    rename files

--- tb/testbench.py
+++ tb/testbench.py
@@ -5,9 +5,9 @@ hdl_sources_dir = Path().parent / "hdl"
 
 def test_quickstart_examples():
     run(
-        verilog_sources=[hdl_sources_dir / "quickstart_examples/my_design.sv"], # sources
+        verilog_sources=[hdl_sources_dir / "00_quickstart_examples/my_design.sv"], # sources
         toplevel="my_design",            # top level HDL
-        module="tb.quickstart_examples.test_my_design"        # name of cocotb test module
+        module="tb.00_quickstart_examples.test_my_design"        # name of cocotb test module
     )
 
 def test_gates():
