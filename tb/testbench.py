7823f0e29807ec06a54d04461611d662ab7ae714
Lukas Weidenholzer <lukas.weidenholzer@gmail.com>
2022-10-01 11:12:43 +0200

    rename testbench file

--- tb/testbench.py
+++ tb/testbench.py
@@ -4,7 +4,7 @@ from cocotb_test.simulator import run
 
 hdl_sources_dir = Path().parent / "hdl"
 
-def test_quickstart_examples():
+def testbench():
     run(
         verilog_sources=[hdl_sources_dir / "quickstart_examples/my_design.sv"], # sources
         toplevel="my_design",            # top level HDL
