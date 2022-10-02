136650fd50fc64371ae81569bb5a81460293e159
Lukas Weidenholzer <lukas.weidenholzer@gmail.com>
2022-10-02 11:02:50 +0200

    fix tests for non

--- tb/01_gates/test_gates.py
+++ tb/01_gates/test_gates.py
@@ -6,18 +6,19 @@ from collections import namedtuple
 
 @cocotb.test()
 async def test_not(dut):
-    TruthTableEntry = namedtuple("TruthTableEntry", "input", "output")
+    TruthTableEntry = namedtuple("TruthTableEntry", ["input", "output"])
 
     truth_table = [
         TruthTableEntry(input=0, output=1),
         TruthTableEntry(input=1, output=0),
     ]
 
-    await Timer(time=1)
-    dut.a.value = 0
-    await Timer(time=1)
+    for entry in truth_table:
+        await Timer(time=1)
+        dut.a.value = entry.input
+        await Timer(time=1)
 
-    assert dut.y.value == 1
+        assert dut.y.value == entry.output
 
 
 @cocotb.test()
