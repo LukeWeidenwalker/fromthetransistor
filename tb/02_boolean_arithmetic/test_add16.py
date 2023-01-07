2e9668eb4d0f6c18e69158cee07478798acc23cb
Lukas Weidenholzer <lukas.weidenholzer@gmail.com>
2023-01-07 10:01:27 +0100

    fix test on add16

--- tb/02_boolean_arithmetic/test_add16.py
+++ tb/02_boolean_arithmetic/test_add16.py
@@ -1,6 +1,7 @@
 from random import randint
 import cocotb
 from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
 
 from collections import namedtuple
 
@@ -18,8 +19,7 @@ async def test_add16(dut):
 
         await Timer(time=1)
 
-        expected_value = ~(in_a + in_b) & 0xFFFF
-        actual_value = dut.out.value.integer
+        expected_value = int(bin(in_a + in_b)[2:].zfill(16)[-16:], 2)
+
+        actual_value = dut.out.value
         assert actual_value == expected_value, f"in_a: {bin(in_a)}, in_b: {bin(in_b)} Expected output: {expected_value}, actual output: {actual_value}, type_expected: {type(expected_value)}, type_Actual: {type(actual_value)}"
-
-
